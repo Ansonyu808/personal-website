36908a38f6dbe97cf3bc3f35839e0ceb4912fcaa
anson <ayu808@hawaii.edu>
2020-08-14 21:45:53 -1000

    Added basic elements to page  (Topbar and 3 sections)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { AppBar, Toolbar, IconButton, Button } from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <AppBar color="inherit">
+          <Toolbar>
+            <IconButton style={{ color: "white" }}>Logo</IconButton>
+            <div className="emptySpace"></div>
+            <Button
+              style={{ color: "white" }}
+              onClick={() => this.test("about")}
+            >
+              About
+            </Button>
+            <Button
+              style={{ color: "white" }}
+              onClick={() => this.test("work")}
+            >
+              Work
+            </Button>
+            <Button
+              style={{ color: "white" }}
+              onClick={() => this.test("projects")}
+            >
+              Projects
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <div id="about" className="section">
+          <h1>Anwar Anson Yu</h1>
+          <h3>Software Engineer</h3>
+          <p>
+            Software engineer passionate in providing software solutions to
+            client problems. Excellent problem solver, teamworker, and leader
+            with a strong sense of passion for learning about software
+            development.
+          </p>
+        </div>
+        <div id="work" className="section">
+          <p>Underwriter Laboratories - Transaction Securtiy Associate</p>
+          <p>USDA - Biological Lab Technician</p>
+        </div>
+        <div id="projects" className="section">
+          <p>Best Beans</p>
+          <p>Instagram Web Scraper</p>
+        </div>
+      </div>
+    );
+  }
+  test = (id) => {
+    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+  };
 }
 
 export default App;
